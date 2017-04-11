2c620c16788dfa592a7de4a549a2e800b9751d59
Hong Xiao <hongtao.xiao@unb.ca>
2017-04-11 13:34:18 -0300

    button status and text changed correctly

--- dialog/dialog.js
+++ dialog/dialog.js
@@ -35,9 +35,23 @@ $( function() {
 
   var form = $dialog.find( "form" ).on( "submit", function( event ) {
     event.preventDefault();
-    var addUser = genAddUser('Test User 2');
+    var addUser = genAddUser('Test User 2');  // XXX what use is this?
     addUser();
   });
+
+  $(".start").click(function() {
+    $(this).text("Enabled");
+    $(this).removeClass("btn-primary").addClass("btn-success");
+  });
+
+  $(".stop").click(function() {
+
+    var $start = $(this).siblings(".start");
+    $start.text("Start");
+    $start.prop("disabled", true);
+
+    $(this).text("Stopped");
+  })
 
   $( "#create-user" ).button().on( "click", function() {
     $dialog.data('username', $("#username").text()).dialog( "open" );
