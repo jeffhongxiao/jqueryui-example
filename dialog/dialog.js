b0bfcac437f9c560cd34a87fdc95119e34804ecf
Hong Xiao <hongtao.xiao@unb.ca>
2017-04-11 09:36:16 -0300

    comment out unused text input and logic

--- dialog/dialog.js
+++ dialog/dialog.js
@@ -1,76 +1,33 @@
 'use strict';
 
 $( function() {
-    // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-    name = $( "#name" ),
-    email = $( "#email" ),
-    password = $( "#password" ),
-    allFields = $( [] ).add( name ).add( email ).add( password ),
-    tips = $( ".validateTips" );
-
-  function updateTips( t ) {
-    tips
-      .text( t )
-      .addClass( "ui-state-highlight" );
-    setTimeout(function() {
-      tips.removeClass( "ui-state-highlight", 1500 );
-    }, 500 );
-  }
-
-  function checkLength( o, n, min, max ) {
-    if ( o.val().length > max || o.val().length < min ) {
-      o.addClass( "ui-state-error" );
-      updateTips( "Length of " + n + " must be between " +
-        min + " and " + max + "." );
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  function checkRegexp( o, regexp, n ) {
-    if ( !( regexp.test( o.val() ) ) ) {
-      o.addClass( "ui-state-error" );
-      updateTips( n );
-      return false;
-    } else {
-      return true;
-    }
-  }
+    // var $name = $( "#name" );
+    // var $email = $( "#email" );
+    // var $password = $( "#password" );
+    // var $allFields = $( [] ).add( name ).add( email ).add( password );
 
   function genAddUser(username) {
     var foo = function() {
       var valid = true;
-      allFields.removeClass( "ui-state-error" );
+      // $allFields.removeClass( "ui-state-error" );
 
-      //valid = valid && checkLength( name, "username", 3, 16 );
-      valid = valid && checkLength( email, "email", 6, 80 );
-      valid = valid && checkLength( password, "password", 5, 16 );
-
-      //valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-      valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-      valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+      // $name.val(username);
 
       if ( valid ) {
-        // $( "#users tbody" ).append( "<tr>" +
-        //   "<td>" + username + "</td>" +
-        //   "<td>" + email.val() + "</td>" +
-        //   "<td>" + password.val() + "</td>" +
-        // "</tr>" );
         console.log("username = " + username);
-        dialog.dialog( "close" );
+        $dialog.dialog( "close" );
       }
       return valid;
     };
     return foo;
   }
 
-  var dialog = $( "#dialog-form" ).dialog({
+  var $dialog = $( "#dialog-form" ).dialog({
     autoOpen: false,
     height: 400,
     width: 350,
     modal: true,
+    title: "Current User is: " + $("#username").text(),
     buttons: {
       "Create an account": function() {
         var addUser = genAddUser($(this).data('username'));
@@ -78,23 +35,23 @@ $( function() {
       },
       Cancel: function() {
         console.log("username is: " + $(this).data('username'));
-        dialog.dialog( "close" );
+        $dialog.dialog( "close" );
       }
     },
     close: function() {
       form[ 0 ].reset();
-      allFields.removeClass( "ui-state-error" );
+//      allFields.removeClass( "ui-state-error" );
     }
   });
 
 
-  var form = dialog.find( "form" ).on( "submit", function( event ) {
+  var form = $dialog.find( "form" ).on( "submit", function( event ) {
     event.preventDefault();
     var addUser = genAddUser('Test User 2');
     addUser();
   });
 
   $( "#create-user" ).button().on( "click", function() {
-    dialog.data('username', $("#username").text()).dialog( "open" );
+    $dialog.data('username', $("#username").text()).dialog( "open" );
   });
 } );
